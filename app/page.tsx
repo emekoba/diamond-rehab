965ecfd2948778a1629eade9688efd829a607dc8
Russell Emekoba <70672283+emekoba@users.noreply.github.com>
2023-05-14 14:43:02 +0000

    Update 3 files

--- app/page.tsx
+++ app/page.tsx
@@ -2,21 +2,21 @@ import Header from "@/components/Header";
 import Image from "next/image";
 
 export default function Home() {
-	return (
-		<main className="flex min-h-screen flex-col items-center justify-between">
-			<Header />
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between">
+      <Header />
 
-			<div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-				sderfgsd
-				<Image
-					src="/vercel.svg"
-					alt="Vercel Logo"
-					className="dark:invert"
-					width={100}
-					height={24}
-					priority
-				/>
-			</div>
-		</main>
-	);
+      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+        sderfgsd
+        <Image
+          src="/assets/bg1.jpg"
+          alt="Vercel Logo"
+          className="dark:invert"
+          width={100}
+          height={24}
+          priority
+        />
+      </div>
+    </main>
+  );
 }
