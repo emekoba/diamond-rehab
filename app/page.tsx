ba103546bec8172be35c17e48743c3055f46d34f
emekoba <rjemekoba@gmail.com>
2023-05-16 12:36:10 +0100

    added construction message

--- app/page.tsx
+++ app/page.tsx
@@ -3,20 +3,9 @@ import Image from "next/image";
 
 export default function Home() {
 	return (
-		<main className="flex min-h-screen flex-col items-center justify-between">
-			<Header />
-
-			<div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-				sderfgsd
-				<Image
-					src="/vercel.svg"
-					alt="Vercel Logo"
-					className="dark:invert"
-					width={100}
-					height={24}
-					priority
-				/>
-			</div>
+		<main className="min-h-screen flex-col items-center h-full w-full font-mono grid text-center bg-red">
+			Welcome to Diamond Rehab 👨🏼‍⚕️👩🏽‍⚕️. This Site is currently under construction
+			🚧 🏗️. Check in with us soon‼️
 		</main>
 	);
 }
